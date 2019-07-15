f4643d51937d0bbec2836153db8d153a368c254d
umairx97 <umair.bajwa@hotmail.com>
2019-07-16 04:40:05 +0500

    fixed bugs

--- src/components/LeftSideBar/LeftBar.js
+++ src/components/LeftSideBar/LeftBar.js
@@ -1,52 +1,55 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGlobeAsia, faKey, faTerminal, faUserFriends, faLock, faArrowAltCircleUp, faAlignLeft } from '@fortawesome/free-solid-svg-icons'
 
 const LeftBar = () => {
     return (
         <div className="site-left-part">
-        <div className="site-left-part-content">
-            <span className="menu-open-trg"><i className="fal fa-align-left" /></span>
-            <div className="left-part-menu">
-                <br />
-                <div className="menu-item">
-                    <ul>
-                        <li>
-                            <a href="">
-                                <i className="fas fa-globe-asia" /> &nbsp; General
+            <div className="site-left-part-content">
+                <span className="menu-open-trg">  <FontAwesomeIcon icon={faAlignLeft} /></span>
+                <div className="left-part-menu">
+                    <br />
+                    <div className="menu-item">
+                        <ul>
+                            <li>
+                                <a href="">
+                                    <FontAwesomeIcon icon={faGlobeAsia} /> &nbsp; General
                              </a>
-                            <hr />
-                        </li>
-                        <li>
-                            <a href="">
-                                <i className="fas fa-key" /> &nbsp; Keys
+                                <hr />
+                            </li>
+                            <li>
+                                <a href="">
+                                    <FontAwesomeIcon icon={faKey} /> &nbsp; Keys
                             </a>
-                            <hr />
-                        </li>
-                        <li>
-                            <a href="">
-                                <i className="fas fa-terminal" /> &nbsp; API
+                                <hr />
+                            </li>
+                            <li>
+                                <a href="">
+                                    <FontAwesomeIcon icon={faTerminal} /> &nbsp; API
                                  </a>
-                            <hr />
-                        </li>
-                        <li><a href=""><i className="fas fa-user-friends" /> &nbsp; Team</a>
-                            <hr />
-                        </li>
-                        <li>
-                            <a href="">
-                                <i className="fas fa-lock" /> &nbsp; Security
+                                <hr />
+                            </li>
+                            <li><a href="">
+                                <FontAwesomeIcon icon={faUserFriends} />&nbsp; Team</a>
+                                <hr />
+                            </li>
+                            <li>
+                                <a href="">
+                                    <FontAwesomeIcon icon={faLock} /> &nbsp; Security
                              </a>
-                            <hr />
-                        </li>
-                        <li>
-                            <a href="">
-                                <i className="fas fa-arrow-alt-circle-up" /> &nbsp; Integrations
+                                <hr />
+                            </li>
+                            <li>
+                                <a href="">
+                                    <FontAwesomeIcon icon={faArrowAltCircleUp} /> &nbsp; Integrations
                                 </a>
-                            <hr />
-                        </li>
-                    </ul>
+                                <hr />
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
     )
 }
 
